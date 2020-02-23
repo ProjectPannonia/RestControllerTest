009139e839dd9b9efb6512ffa75b5490463b0358
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-02-23 11:00:53 +0100

    Getting started

---
+++ src/test/java/com/restControllerTest/RestControllerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restControllerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControllerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
